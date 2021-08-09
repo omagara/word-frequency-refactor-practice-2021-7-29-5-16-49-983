a2727f6c5f0a02b8b7d8d45c93c0fe52f6bebaaf
omagara <omagara@oocl.com>
2021-08-09 21:13:16 +0800

    [WordFrequency][Ramon]refactor: extracted getWordInfoList function from getResult function

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -14,33 +14,37 @@ public class WordFrequencyGame {
             try {
 
                 //split the input string with 1 to n pieces of spaces
-                String[] words = sentence.split(EMPTY_SPACE);
-
-                List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String word : words) {
-                    WordInfo wordInfo = new WordInfo(word, 1);
-                    wordInfoList.add(wordInfo);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordInfo>> map =getListMap(wordInfoList);
-
-                List<WordInfo> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()){
-                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(wordInfo);
-                }
-                wordInfoList = list;
-
-                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                return reconstructWordInfo(wordInfoList);
+                return getWordInfoList(sentence);
             } catch (Exception e) {
 
 
                 return CALCULATE_ERROR;
             }
         }
+
+    private String getWordInfoList(String sentence) {
+        String[] words = sentence.split(EMPTY_SPACE);
+
+        List<WordInfo> wordInfoList = new ArrayList<>();
+        for (String word : words) {
+            WordInfo wordInfo = new WordInfo(word, 1);
+            wordInfoList.add(wordInfo);
+        }
+
+        //get the map for the next step of sizing the same word
+        Map<String, List<WordInfo>> map =getListMap(wordInfoList);
+
+        List<WordInfo> list = new ArrayList<>();
+        for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()){
+            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
+            list.add(wordInfo);
+        }
+        wordInfoList = list;
+
+        wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+
+        return reconstructWordInfo(wordInfoList);
+    }
 
     private String reconstructWordInfo(List<WordInfo> wordInfoList) {
         return wordInfoList.stream()
@@ -51,6 +55,7 @@ public class WordFrequencyGame {
 
     private Map<String,List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
         Map<String, List<WordInfo>> map = new HashMap<>();
+
         for (WordInfo wordInfo : wordInfoList){
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
             if (!map.containsKey(wordInfo.getWord())){
