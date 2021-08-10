34c5f86e3926aeec8367b1dca40a6bf9b6c5ba10
omagara <omagara@oocl.com>
2021-08-11 06:54:38 +0800

    [WordFrequency][Ramon]refactor: extracted sortWordInfo from getWordInfoList

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -11,22 +11,27 @@ public class WordFrequencyGame {
         try {
             List<WordInfo> wordInfoList = getWordInfoList(sentence);
             return reconstructWordInfo(wordInfoList);
-        } catch (Exception e) {
-            return CALCULATE_ERROR;
+        } catch (Exception e)  {
+            return CALCULATE_ERROR; // TODO: customized exception
         }
     }
 
     private List<WordInfo> getWordInfoList(String sentence) {
         List<String> words = Arrays.asList(sentence.split(EMPTY_SPACE));
         List<WordInfo> wordInfoList = new ArrayList<>();
-
+            //TODO: use stream map
         for (String word : new HashSet<>(words)) {
             int count = Collections.frequency(words, word);
             wordInfoList.add(new WordInfo(word, count));
         }
-        wordInfoList.sort((firstWordInfo, secondWordInfo) -> secondWordInfo.getWordCount() - firstWordInfo.getWordCount());
+        sortWordInfo(wordInfoList);
+
 
         return wordInfoList;
+    }
+
+    private void sortWordInfo(List<WordInfo> wordInfoList) {
+        wordInfoList.sort((firstWordInfo, secondWordInfo) -> secondWordInfo.getWordCount() - firstWordInfo.getWordCount());
     }
 
     private String reconstructWordInfo(List<WordInfo> wordInfoList) {
